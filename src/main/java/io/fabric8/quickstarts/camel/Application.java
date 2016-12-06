fc3cecd9784d60160aa70e8778b1d74df3fa91a2
eformat <eformat@gmail.com>
2016-12-06 17:15:05 +1000

    hot reload

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,33 +15,34 @@
  */
 package io.fabric8.quickstarts.camel;
 
-//import org.apache.camel.builder.RouteBuilder;
+
+import org.apache.camel.builder.RouteBuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application { //extends RouteBuilder {
+public class Application extends RouteBuilder {
 
-    //@Autowired
-    //ApplicationConfig config;
+    @Autowired
+    ApplicationConfig config;
     
     // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-/*    @Override
+    @Override
     public void configure() throws Exception {
         from("timer://foo?period=5000")
             .setBody().constant(config.getMessage())
             .log(">>> ${body}");
     }
-*/
+
  }
