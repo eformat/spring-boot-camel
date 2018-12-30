df8f53a43e0264093fea5651da5693caccd21922
Mike Hepburn <eformat@gmail.com>
2018-12-30 14:58:01 +1000

    udated for fuse7

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -1,35 +1,32 @@
 /**
- *  Copyright 2005-2016 Red Hat, Inc.
+ * Copyright 2005-2016 Red Hat, Inc.
  *
- *  Red Hat licenses this file to you under the Apache License, version
- *  2.0 (the "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ * Red Hat licenses this file to you under the Apache License, version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- *  implied.  See the License for the specific language governing
- *  permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.fabric8.quickstarts.camel;
 
-//import org.apache.camel.builder.RouteBuilder;
-//import org.springframework.beans.factory.annotation.Autowired;
+// import org.apache.camel.builder.RouteBuilder;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
 @SpringBootApplication
-//@ImportResource({"classpath:spring/camel-context.xml"})
+// @ImportResource({"classpath:spring/camel-context.xml"})
 public class Application {
 
     // must have a main method spring-boot can run
@@ -37,18 +34,18 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+
 @Component
 class HelloRouter extends RouteBuilder {
- 
+
     @Autowired
     ApplicationConfigBean appConfig;
 
     @Override
     public void configure() throws Exception {
-        from("timer://foo?period=" + appConfig.getTime())
-        .setBody()
-        .constant(appConfig.getMessage())
-        .log(">>> ${body}");
+        from("timer://foo?period=" + appConfig.getTime()).setBody().constant(appConfig.getMessage())
+                .log(">>> ${body}");
     }
 
 }
